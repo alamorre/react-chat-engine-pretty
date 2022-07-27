79ecda0f6cf3f5e03df3cc5c877ede27c319bed7
Adam La Morre <alamorre@gmail.com>
2022-07-26 20:00:41 -0700

    Chat UI is in!

--- example/src/App.js
+++ example/src/App.js
@@ -3,7 +3,13 @@ import { PrettyChatWindow } from 'react-chat-engine-advanced';
 function App() {
   return (
     <div>
-      <PrettyChatWindow />
+      <PrettyChatWindow
+        projectId="e9cffdf0-9612-4ec3-a8f1-343938280012"
+        username="adam@lamorre.co"
+        secret="Pass1234!"
+        httpUrl="http://127.0.0.1:8000"
+        height="calc(100vh - 24px)"
+      />
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,35 @@
 import * as React from 'react';
 
-export const PrettyChatWindow = () => {
-  return <div>the snozzberries taste like snozzberries!</div>;
+import {
+  useMultiChatLogic,
+  MultiChatWindow,
+  MultiChatWindowProps,
+  MultiChatSocket,
+} from 'react-chat-engine-advanced';
+
+interface PrettyChatWindowProps extends MultiChatWindowProps {
+  projectId: string;
+  username: string;
+  secret: string;
+  httpUrl?: string;
+  height?: string;
+}
+
+export const PrettyChatWindow = (props: PrettyChatWindowProps) => {
+  const chatProps = useMultiChatLogic(
+    props.projectId,
+    props.username,
+    props.secret,
+    props.httpUrl
+  );
+
+  return (
+    <div style={{ height: props.height ? props.height : '100%' }}>
+      <MultiChatSocket {...chatProps} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: props.height ? props.height : '100%' }}
+      />
+    </div>
+  );
 };
